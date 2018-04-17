2070b36670a9fa4cec41be3d9784ac0e495d3122
Smcompte <smcompte@yahoo.fr>
2018-04-17 09:24:15 +0200

    Commit and push directely

--- gitproject/src/main/java/fr/git/MainClass.java
+++ gitproject/src/main/java/fr/git/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		System.out.println("First project shared to git");
+		System.out.println("First commit and push to git");
 	}
 	
 }
