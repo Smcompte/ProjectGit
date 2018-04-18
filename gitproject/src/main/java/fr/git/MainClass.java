2c9e86f56818e57f57b8cc12971e4fc87dd0f0d7
Smcompte <smcompte@yahoo.fr>
2018-04-18 16:52:53 +0200

    18 avril 16h53

--- gitproject/src/main/java/fr/git/MainClass.java
+++ gitproject/src/main/java/fr/git/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("First project shared to git");
 		System.out.println("First commit and push to git");
+		System.out.println("Annother commit : 18-avril 16h53");
 	}
 	
 }
